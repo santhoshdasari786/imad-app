12054c248c06c8f7d41304c7cb289a3c4a638861
santhoshdasari786 <30649656+santhoshdasari786@users.noreply.github.com>
2017-08-09 22:32:58 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,9 @@
 console.log('Loaded!');
+
+var counter = 0;
+var button = document.getElementById("counter");
+var span = document.getElementById("count");
+span.innerHTML = counter.toString();
+button.onclick = function(){
+    counter = counter+1;
+}
