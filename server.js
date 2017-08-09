89f68a57018f6c5649868de867544224e0791ddf
santhoshdasari786 <30649656+santhoshdasari786@users.noreply.github.com>
2017-08-09 21:26:42 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -82,6 +82,10 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/profile', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'profile.html'));
+});
+
 app.get('/articleone', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'articleone.html'));
 });
