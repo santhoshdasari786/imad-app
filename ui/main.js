a0481e14d507765d4af6a3ed924898d0774604b1
santhoshdasari786 <30649656+santhoshdasari786@users.noreply.github.com>
2017-08-11 22:59:19 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -39,14 +39,30 @@ var submit = document.getElementById('submit_btn');
 
 submit.onclick = function ()
 {
-      var names = ['name1','name2','name3',"name4"]; 
-      var list = '';
+    var nameInput = document.getElementById('name');
+    var name = nameInput.value;
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function() {
+        if(request.readyState === XMLHttpRequest.DONE)
+        {
+             if(request.status === 200)
+             {
+                 var names = request.responseText; 
+                 names = JSON.parse(names);
+                 var list = '';
       for(i=0; i<names.length ; i++)
       {
           list+= '<li>' +names[i] + '<li>';
       }
       var ul = document.getElementById('namelist');
       ul.innerHTML = list;
+                 
+             }
+        }
+    };
+    // make request 
+    request.open('GET','http://santhoshdasari786.imad.hasura-app.io/submit-name?name=' + name ,true);
+    request.send(null);
 };
 
 
