fc94841d1798c0471bb4b7b85ecd78ee3f2f01a1
santhoshdasari786 <30649656+santhoshdasari786@users.noreply.github.com>
2017-08-11 21:31:44 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -39,7 +39,7 @@ var submit = document.getElementById('submit_btn');
 
 submit.onclick = function ()
 {
-      var names = ['name1','name2','name3'];
+      var names = ['name1','name2','name3',"name4"];
       var list = '';
       for(i=0;i<names.length;i++)
       {
