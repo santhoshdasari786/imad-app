3ff3fc0fa5720cd350ecf9523560c0d39bc67e1b
santhoshdasari786 <30649656+santhoshdasari786@users.noreply.github.com>
2017-08-16 18:48:50 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 var Pool = require('pg').Pool;
+var crypto = require('crypto');
 
 var config = {
     user: 'santhoshdasari786',
@@ -180,6 +181,18 @@ app.get('/articles/:articlewo', function (req, res) {
        }
     });
  
+});
+
+function hash (input,salt )
+{
+  var hashed = crypto.pbkdf2Sync(input,salt,10000, 512, 'sha512');
+  return hashed.toString('hex');
+}
+
+app.get('/hash/:input', function(req, res)
+{
+    var hashedString = hash(req.params.input,'this-is-some-random-string');
+    res.send(hashedString);
 });
 
 app.get('/:articlewo', function (req, res) {
