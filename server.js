62ae0509600cc46c52eb6b3a3db0c29b4d7f1b0d
santhoshdasari786 <30649656+santhoshdasari786@users.noreply.github.com>
2017-08-16 08:25:20 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var pool = require('pg').pool;
+
+var config = {
+    user: 'santhoshdasari786',
+    database: 'santhoshdasari786',
+    host:'db.imad.hasura-app.io',
+    port:'5432',
+    password: process.env.DB_PASSWORD
+}
 
 var app = express();
 app.use(morgan('combined'));
@@ -30,6 +39,7 @@ function createTemplate (data) {
 var title = data.title;
 var heading = data.heading;
 var content = data.content;
+
 
 var htmltamplate = `
 <html>
@@ -82,6 +92,23 @@ var htmltamplate = `
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+
+var pool = new Pool(config);
+
+app.get('/test-db', function(req,res)
+{
+    pool.query('SELECT * FROM test', function(err,result){
+       if(err)
+       {
+           res.status(500).send(err.toString());
+       }
+       else
+       {
+           res.send(JSON.stringify(result));
+       }
+    });
 });
 
 app.get('/ui/main.js', function (req, res) {
