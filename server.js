49b998569e784c95faad6faec4640958814bd6d0
santhoshdasari786 <30649656+santhoshdasari786@users.noreply.github.com>
2017-08-22 12:39:55 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,42 @@ var Pool = require('pg').Pool;
 var crypto = require('crypto');
 var bodyParser = require('body-parser');
 
+
+import {
+  graphql,
+  GraphQLSchema,
+  GraphQLObjectType,
+  GraphQLString
+} from 'graphql';
+
+var schema = new GraphQLSchema({
+  query: new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: {
+      hello: {
+        type: GraphQLString,
+        resolve: () => 'world'
+      }
+    }
+  })
+});
+
+var query = `
+  query HelloQuery {
+    { hello }
+  }
+`;
+
+graphql(schema, query).then((result) => {
+  // Prints
+  // {
+  //   data: { hello: "world" }
+  // }
+  console.log(result);
+});
+
+
+
 var config = {
     user: 'santhoshdasari786',
     database: 'santhoshdasari786',
