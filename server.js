44b340917c4737cfca62e9e1bc404c51a5f41b77
santhoshdasari786 <30649656+santhoshdasari786@users.noreply.github.com>
2017-08-22 14:12:37 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,46 +6,8 @@ var crypto = require('crypto');
 var bodyParser = require('body-parser');
 
 
-//require('babel');
-//require('request');
-// {request} from 'request';
-//require('graphql');
 var graphqlHTTP = require('express-graphql');
 var  buildSchema  = require('graphql');
-
-
-/*import {
-  graphql,
-  GraphQLSchema,
-  GraphQLObjectType,
-  GraphQLString
-} from 'graphql';*/
-
-var schema = new GraphQLSchema({
-  query: new GraphQLObjectType({
-    name: 'RootQueryType',
-    fields: {
-      hello: {
-        type: GraphQLString,
-        resolve: () => 'world'
-      }
-    }
-  })
-});
-
-var query = `
-  query HelloQuery {
-    { hello }
-  }
-`;
-
-graphql(schema, query).then((result) => {
-  // Prints
-  // {
-  //   data: { hello: "world" }
-  // }
-  console.log(result);
-});
 
 
 
