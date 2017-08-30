c4bfa21abea6dcd0ba0182ad8badcec144f9cbbc
santhoshdasari786 <30649656+santhoshdasari786@users.noreply.github.com>
2017-08-30 11:51:15 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -302,7 +302,7 @@ app.post('/login', function(req,res){
    
 });
 
-app.get('/check-login', function(req, res) {
+app.get('/checklogin', function(req, res) {
    if ( req.session && req.session.auth && req.session.auth.userId)
    {
        res.send('you are logged in : ' + req.session.auth.userId.toString());
