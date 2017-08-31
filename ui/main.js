ce77b5241e08e9f1b344739027502fc209f68fbc
santhoshdasari786 <30649656+santhoshdasari786@users.noreply.github.com>
2017-08-31 18:27:25 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -105,4 +105,41 @@ signin.onclick = function()
     
 };
 
+var signup = document.getElementById('sign_up');
+
+signup.onclick = function() 
+{
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+    console.log(username);
+     console.log(password);
+    
+    var request = new XMLHttpRequest();
+    
+    request.onreadystatechange = function () {
+        if(request.readyState === XMLHttpRequest.DONE)
+        {
+            if(request.status === 200)
+            {
+                console.log('user is signedup');
+                alert('User SignedUp successfully');
+            }
+            else if(request.status === 403)
+            {
+                   alert('username/password invalid');
+            }
+            else if(request.status === 500)
+            {
+                   alert('something went wrong');
+            }
+        }
+    };
+    
+    
+    request.open('POST','http://santhoshdasari786.imad.hasura-app.io/create-user' ,true);
+    request.setRequestHeader('Content-Type','application/json');
+    request.send(JSON.stringify({username: username, password: password}));
+    
+};
+
 
